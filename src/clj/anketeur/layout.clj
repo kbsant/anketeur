683f47ef6b8d5cd2efa943b4f9839cd57886b7ae
keansant <keansant@gmail.com>
2018-04-10 04:59:59 +0900

    clean up error page

--- src/clj/anketeur/layout.clj
+++ src/clj/anketeur/layout.clj
@@ -2,7 +2,6 @@
   (:require
     [anketeur.view.error :as view.error]
     [ring.util.http-response :refer [content-type ok]]
-    [ring.util.anti-forgery :refer [anti-forgery-field]]
     [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]))
 
 (declare ^:dynamic *app-context*)
@@ -20,14 +19,7 @@
     "text/html; charset=utf-8"))
 
 (defn error-page
-  "error-details should be a map containing the following keys:
-   :status - error status
-   :title - error title (optional)
-   :message - detailed error message (optional)
-
-   returns a response map with the error page as the body
-   and the status specified by the status key"
-  [error-details]
-  {:status  (:status error-details)
+  [{:keys [status] :as details}]
+  {:status  status
    :headers {"Content-Type" "text/html; charset=utf-8"}
-   :body    (view.error/render {:glossary error-details})})
+   :body    (view.error/render {:glossary details})})
