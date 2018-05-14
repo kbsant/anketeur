260113c4cf6fdc97fbbecdc5c7461e8dfa693661
kbsant <keansant@gmail.com>
2018-05-14 19:50:36 +0900

    tile menu

--- src/clj/anketeur/view/main.clj
+++ src/clj/anketeur/view/main.clj
@@ -8,9 +8,17 @@
     (form/navbar [:h1 appname])
     (when message [:p message])
     [:ul
-      [:li [:a {:href "/open"} "Create or edit a survey"]]
-      [:li [:a {:href "/answer"} "Respond to a survey"]]
-      [:li [:a {:href "/result"} "Gather results"]]]])
+     [:li "TODO"
+      [:ul
+       [:li "login"]]]]
+    [:div.container
+      [:div.row
+        [:div.col-xs-4.main-tile
+           [:a {:href "/open"} [:img {:src "/img/file.png"}] [:br] [:p "Create or edit a survey"]]]
+        [:div.col-xs-4.main-tile
+          [:a {:href "/answer"} [:img {:src "img/entrance.png"}] [:br] [:p "Respond to a survey"]]]
+        [:div.col-xs-4.main-tile
+           [:a {:href "/result"} [:img {:src "img/calculator.png"}] [:br] [:p "Gather results"]]]]]])
 
 (defn render [{:keys [glossary]}]
   (parts/main glossary 
