dcb1583b70c7abed6f56eb3e4806aef524e5ddfe
kbsant <keansant@gmail.com>
2018-08-02 12:58:50 +0900

    document and format routes

--- src/clj/anketeur/route/home.clj
+++ src/clj/anketeur/route/home.clj
@@ -7,10 +7,11 @@
     [anketeur.middleware :as middleware]))
 
 (defn- applyfn
-  "Return a function that calls the given function with its arguments.
-  The returned function ignores its parameter.
-  This is needed because the shorthand function syntax #() produces a zero-arity function,
-  while we need a 1-arity function for the route definition."
+  "A short form of (fn [_]( ... )).
+  This returns a function that calls the given function with its arguments.
+  The returned function is arity-1, but ignores its argument.
+  We use this when we want to supply a handler (which must have arity-1)
+  to the route definition, but we don't actually need the argument."
   ([f x] (fn [_] (f x)))
   ([f x & args] (fn [_] (apply f x args))))
 
@@ -18,33 +19,41 @@
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
-   ["/" (applyfn main/render env)]
-   ["/fileaction" {:post (partial edit/file-action ds)}]
-   ["/save" {:post (partial edit/save-action ds)}]
-   ["/answer" {:get (applyfn answer/render-opener ds)
-               :post (partial answer/answer-action ds)}]
-   ["/answernojs" {:post (partial answer/answernojs-action ds)}]
+   ["/"
+    (applyfn main/render env)]
+   ["/fileaction"
+    {:post (partial edit/file-action ds)}]
+   ["/save"
+    {:post (partial edit/save-action ds)}]
+   ["/answer"
+    {:get (applyfn answer/render-opener ds)
+     :post (partial answer/answer-action ds)}]
+   ["/answernojs"
+    {:post (partial answer/answernojs-action ds)}]
    ["/answer/completed/:surveyno"
     (fn [{{:keys [surveyno]} :path-params}]
       (answer/completed ds surveyno))]
    ["/answer/id/:surveyno"
     (fn [{{:keys [surveyno]} :path-params}]
       (answer/render-add ds surveyno))]
-   ["/answer/add" {:post (partial answer/add-action ds)}]
+   ["/answer/add"
+    {:post (partial answer/add-action ds)}]
    ["/answer/id/:surveyno/formno/:formno"
     (fn [{{:keys [surveyno formno]} :path-params}]
       (answer/render-responder ds surveyno formno))]
    ["/answernojs/id/:surveyno/formno/:formno"
     (fn [{{:keys [surveyno formno]} :path-params}]
       (answer/render-responder-nojs ds surveyno formno))]
-   ["/open" (applyfn edit/render-opener ds)]
+   ["/open"
+    (applyfn edit/render-opener ds)]
    ["/edit/id/:surveyno"
     (fn [{{:keys [surveyno]} :path-params}]
       (edit/render-editor ds surveyno))]
    ["/edit/export/EDN/id/:surveyno"
     (fn [{{:keys [surveyno]} :path-params}]
       (edit/export ds surveyno))]
-   ["/result" (applyfn result/render-opener ds)]
+   ["/result"
+    (applyfn result/render-opener ds)]
    ["/result/id/:surveyno"
     (fn [{{:keys [surveyno]} :path-params}]
       (result/render-result ds surveyno))]
