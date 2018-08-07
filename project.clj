98c392cae17bba440122c8eba359407e27a748ef
kbsant <keansant@gmail.com>
2018-08-08 04:33:16 +0900

    decouple dependencies

--- project.clj
+++ project.clj
@@ -3,33 +3,9 @@
   :description "A simple app for conducting surveys"
   :url "http://www.ashikasoft.com"
 
-  :dependencies [[clj-time "0.14.4"]
-                 [cljs-ajax "0.7.4"]
-                 [com.cognitect/transit-java "0.8.332"]
-                 [cprop "0.1.11"]
-                 [crypto-password "0.2.0"]
-                 [integrant "0.6.3"]
-                 [luminus-immutant "0.2.4"]
-                 [luminus-nrepl "0.1.4"]
-                 [luminus/ring-ttl-session "0.3.2"]
-                 [metosin/muuntaja "0.5.0"]
-                 [metosin/reitit "0.1.3"]
-                 [metosin/ring-http-response "0.9.0"]
-                 [org.apache.commons/commons-text "1.4"]
-                 [org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]
+  :dependencies [[ashikasoft/webstack "0.1.0-SNAPSHOT"]
                  [org.clojure/clojurescript "1.10.339" :scope "provided"]
-                 [org.clojure/data.codec "0.1.1"]
-                 [org.clojure/tools.cli "0.3.7"]
-                 [org.clojure/tools.logging "0.4.1"]
-                 [org.webjars.bower/tether "1.4.4"]
-                 [org.webjars/bootstrap "4.1.1"]
-                 [org.webjars/font-awesome "5.1.0"]
-                 [org.webjars/webjars-locator "0.34"]
-                 [reagent "0.8.1"]
-                 [ring-webjars "0.2.0"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [reagent "0.8.1"]]
 
   :min-lein-version "2.0.0"
   
