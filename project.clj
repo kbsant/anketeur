b9fe7c2d8daef4b27b20a37061b6f80a1f0ccea6
kbsant <keansant@gmail.com>
2021-12-12 09:27:27 +0900

    bump deps

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :description "A simple app for conducting surveys"
   :url "http://www.ashikasoft.com"
 
-  :dependencies [[ashikasoft/filestore "0.1.2"]
-                 [ashikasoft/webstack "0.1.4"]
-                 [clojure.java-time "0.3.2"]
-                 [org.clojure/clojure "1.10.2"]
-                 [org.clojure/clojurescript "1.10.773" :scope "provided"]]
+  :dependencies [[ashikasoft/filestore "0.1.3"]
+                 [ashikasoft/webstack "0.1.7"]
+                 [clojure.java-time "0.3.3"]
+                 [org.clojure/clojure "1.10.3"]
+                 [org.clojure/clojurescript "1.10.896" :scope "provided"]]
 
   :min-lein-version "2.0.0"
   
@@ -55,14 +55,14 @@
    :test          [:project/dev :project/test :profiles/test]
 
    :project/dev  {:jvm-opts ["-Dconf=dev-config.edn"]
-                  :dependencies [[binaryage/devtools "1.0.2"]
+                  :dependencies [[binaryage/devtools "1.0.4"]
                                  [com.cemerick/piggieback "0.2.2"]
                                  [doo "0.1.11"]
-                                 [expound "0.8.9"]
+                                 [expound "0.8.10"]
                                  [figwheel-sidecar "0.5.20"]
-                                 [pjstadig/humane-test-output "0.10.0"]
-                                 [prone "2020-01-17"]
-                                 [ring/ring-devel "1.9.0"]
+                                 [pjstadig/humane-test-output "0.11.0"]
+                                 [prone "2021-04-23"]
+                                 [ring/ring-devel "1.9.4"]
                                  [ring/ring-mock "0.4.0"]]
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.23.0"]
                                  [lein-doo "0.1.11"]
